a7ee56ed242b3365d0781ac771cf1ed4d0bbd3e9
Lola <86410482+19Lola89@users.noreply.github.com>
2023-02-24 19:34:40 +0000

    files

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,53 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// array of questions for user>
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe your project",
+  },
+  {
+    type: "list",
+    name: "contents",
+    message: "Please add items to your table of contents",
+  },
+  {
+    type: "input",
+    name: "hobby",
+    message: "What is your favorite hobby?",
+  },
+]);
 
-];
+// // function to write README file
+// function writeToFile(fileName, data) {}
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to initialize program
+// function init() {}
 
-// function to initialize program
-function init() {
+// // function call to initialize program
+// init();
 
-}
+//
 
-// function call to initialize program
-init();
+// Table of Contents
+
+// Installation
+
+// Usage
+
+// License
+
+// Contributing
+
+// Tests
+
+// Questions
